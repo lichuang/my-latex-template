6402ae395986989ad9047a84da0852e4c37bf78b
codedump <lichuang1982@gmail.com>
2024-04-07 23:12:34 +0800

    add codedumpnote.cls

--- mklink.sh
+++ mklink.sh
@@ -1,4 +1,6 @@
 rm codedump.cls
+rm codedumpnote.cls
 rm AJbook.cls
 ln -s ../my-latex-template/codedump.cls codedump.cls
 ln -s ../my-latex-template/AJbook.cls AJbook.cls
+ln -s ../my-latex-template/codedumpnote.cls codedumpnote.cls
