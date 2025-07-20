222bedf835366f10e33a8347043635d833a837f9
codedump <lichuang1982@gmail.com>
2025-07-20 11:48:34 +0800

    ci

--- mklink.sh
+++ mklink.sh
@@ -1,6 +1,8 @@
 rm codedump.cls
 rm codedumpnote.cls
 rm AJbook.cls
+rm lc-elegantbook.cls
 #ln -s ../my-latex-template/codedump.cls codedump.cls
 #ln -s ../my-latex-template/AJbook.cls AJbook.cls
 ln -s ../my-latex-template/codedumpnote.cls codedumpnote.cls
+ln -s ../my-latex-template/lc-elegantbook.cls lc-elegantbook.cls
